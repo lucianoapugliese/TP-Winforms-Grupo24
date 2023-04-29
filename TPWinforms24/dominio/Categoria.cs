10cda262485795f75bf85ed4d2bb9f3bbe0e238b
MatiasSilvaIslas <matias.silva@alumnos.frgp.utn.edu.ar>
2023-04-29 16:59:15 -0300

    Agrego la clase categoría

---
+++ TPWinforms24/dominio/Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace dominio
+{
+    class Categoria
+    {
+        public int ID { get; set; }
+        [DisplayName("Número")]
+        public string Descripcion { get; set; }
+
+    }
+}
