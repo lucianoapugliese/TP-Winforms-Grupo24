9c70a0b69863bf661775287ce1daf08e5794b44b
Majo <illanes.mariajose@gmail.com>
2023-04-30 19:34:27 -0300

    Agrego clase Marca

---
+++ TPWinforms24/dominio/Marca.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Marca
+    {
+        private int Id;
+        private int IdArticulo;
+        private string ImagenUrl;
+    }
+}
