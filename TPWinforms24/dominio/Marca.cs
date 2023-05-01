b04cdff225b4fbcebe1bba8f072515a6ff95ce33
Luciano Apugliese <luciano.apugliese@alumnos.frgp.utn.edu.ar>
2023-04-30 21:58:47 -0300

    Agregada clase Articulo y correcciones en clase Marca

--- TPWinforms24/dominio/Marca.cs
+++ TPWinforms24/dominio/Marca.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,9 @@ namespace dominio
 {
     public class Marca
     {
-        private int Id;
-        private int IdArticulo;
-        private string ImagenUrl;
+        [DisplayName("Número")]
+        public int Id { get; set; }
+        public int IdArticulo { get; set; }
+        public string ImagenUrl { get; set; }
     }
 }
