e4606ba8fb20a744efff7c68db6c3dc197cae73f
Majo <illanes.mariajose@gmail.com>
2023-05-01 14:01:54 -0300

    marcaNegocio ok

---
+++ TPWinforms24/Negocio/marcaNegocio.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace Negocio
+{
+    public class marcaNegocio
+    {
+
+        public List<Marca> listar()
+        {
+            List<Marca> marcas = new List<Marca>();
+            AccesoDatos acceso = new AccesoDatos();
+
+            try
+            {
+                acceso.setearConsulta("SELECT Id, Descripcion FROM MARCAS");
+                acceso.ejecutarLectura();
+
+                while (acceso.Lector.Read())
+                {
+                    Marca aux = new Marca();
+                    aux.Id = (int)acceso.Lector["Id"];
+                    aux.Descripcion = (string)acceso.Lector["Descripcion"];
+                    marcas.Add(aux);
+                }
+
+                return marcas;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                acceso.cerrarConexion();
+            }
+        }
+
+        /*
+         * listar
+         * editar
+         * crear
+         * eliminar
+         */
+    }
+}
