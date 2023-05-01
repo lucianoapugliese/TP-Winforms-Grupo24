776928f1c3a0161fd3536477fc4a95705c1cf8d6
MatiasSilvaIslas <matias.silva@alumnos.frgp.utn.edu.ar>
2023-04-30 21:08:14 -0300

    Ajustes menores

--- TPWinforms24/TPWinforms24/Form1.cs
+++ TPWinforms24/TPWinforms24/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using dominio;
+using Negocio;
 
 namespace TPWinforms24
 {
@@ -15,6 +17,10 @@ namespace TPWinforms24
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
         }
     }
 }
